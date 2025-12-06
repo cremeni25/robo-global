815758ba1ed83e17732215d3b641fbd3642ee683
cremeni25 <andersonnavarro.si@gmail.com>
2025-12-06 19:16:30 -0300

    Create supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,11 @@
+from supabase import create_client
+import os
+
+def get_supabase():
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+
+    if not url or not key:
+        raise ValueError("SUPABASE_URL ou SUPABASE_KEY não configurados.")
+
+    return create_client(url, key)
